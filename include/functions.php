8bab475183545db8f878ec1f569586b22d066d4e
Musadiq Mustafa <musadiqmustafa48@gmail.com>
2023-03-10 05:14:40 +0500

    added some php

--- include/functions.php
+++ include/functions.php
@@ -1,11 +1,15 @@
 <?php
 if ($_POST["action"] == "login") {
     login();
-}
-else{}
-function login(){
-    include ('../include/db_connection.php');
-    $arr=[];
+} else if ($_POST["action"] == "logout") {
+    logout();} 
+else if ($_POST["action"] == "change_password") {
+    change_password();}
+
+function login()
+{
+    include '../include/db_connection.php';
+    $arr = [];
     $id = $_POST["id"];
     $password = $_POST["password"];
     $sql = "SELECT * FROM `user` WHERE `name` = '$id' AND `password` = '$password'";
@@ -14,32 +18,25 @@ function login(){
         session_start();
         $_SESSION["status"] = "logged in";
         $_SESSION["id"] = $id;
-        array_push($arr,'logged in');
-        while($row = $result->fetch_assoc()) {
+        array_push($arr, 'logged in');
+        while ($row = $result->fetch_assoc()) {
             $_SESSION["name"] = $row["name"];
-            array_push($arr,$row['name']);
+            array_push($arr, $row['name']);
 
         }
-        
-       
 
         echo json_encode($arr);
-      
-        
-        
-      
-       
-        
-       
 
-        
-        
-        
-       
-        
-        
     } else {
         echo "0 results";
     }
 }
-?>
+
+function logout()
+{
+    session_start();
+    session_destroy();
+    echo 'logout';
+}
+function change_password(){
+echo "password changed";}
